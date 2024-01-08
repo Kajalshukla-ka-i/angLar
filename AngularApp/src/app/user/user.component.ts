928e34e2b491fa6ff5c441e06c5bd9bf9f81c60a
kajal <122118273+Kajalshukla-ka-i@users.noreply.github.com>
2024-01-08 17:31:19 +0530

    .

--- AngularApp/src/app/user/user.component.ts
+++ AngularApp/src/app/user/user.component.ts
@@ -8,54 +8,58 @@ import { Users } from './users.modal';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit{
-  user_value:any;
-  api_value:any;
-  target:string ='';
+export class UserComponent implements OnInit {
+  user_value: any;
+  api_value: any;
+  target: string = '';
   userobj = new Users()
-  constructor(private userdata:UserDataService){}
+  constructor(private userdata: UserDataService) { }
 
-  ngOnInit():void{
-   this.user_value= this.userdata.getUserData();
-  //  console.log(this.user_value);
+  ngOnInit(): void {
+    this.user_value = this.userdata.getUserData();
+    //  console.log(this.user_value);
 
-   this.showAPIDATA();
+    this.showAPIDATA();
   }
 
-  showAPIDATA(){
-    this.userdata.getDataFromAPI().subscribe(res=>{
+  showAPIDATA() {
+    this.userdata.getDataFromAPI().subscribe(res => {
       console.log(res);
-      this.api_value=res;
+      this.api_value = res;
     })
   }
 
-  add_Student(){
+  add_Student() {
     // console.log(this.userobj);
-    this.userdata.AddDataFromAPI(this.userobj).subscribe((response: any)=>{
+    this.userdata.AddDataFromAPI(this.userobj).subscribe((response: any) => {
       this.showAPIDATA();//refresh data
-      this.userobj.name ='';
-      this.userobj.class='';
-      this.userobj.email='';
+      this.userobj.name = '';
+      this.userobj.class = '';
+      this.userobj.email = '';
       console.log(response);
-      if(response.code ==1){
-        this.target = '<div class="alert alert-success"> '+response.message+'</div>'
-      }else if(response.code==2){
-        this.target = '<div class="alert alert-warning"> '+response.message+'</div>'
+      if (response.code == 1) {
+        this.target = '<div class="alert alert-success"> ' + response.message + '</div>'
+      } else if (response.code == 2) {
+        this.target = '<div class="alert alert-warning"> ' + response.message + '</div>'
       }
     });
   }
 
-  delete_data(id:any){
-    this.userdata.DeleteDataFromAPI(id).subscribe((response:any) =>{
-      console.log('delete successful');
-      this.showAPIDATA();
-      console.log(response);
-      if(response.code ==1){
-        this.target = '<div class="alert alert-success"> '+response.message+'</div>'
-      }else if(response.code==2){
-        this.target = '<div class="alert alert-warning">'+response.message+'</div>'
-      }
-    });
+  delete_data(id: any) {
+    var c = confirm('Are you sure you want to delete this record ?');
+    if (c) {
+      this.userdata.DeleteDataFromAPI(id).subscribe((response: any) => {
+        console.log('delete successful');
+        this.showAPIDATA();
+        console.log(response);
+        if (response.code == 1) {
+          this.target = '<div class="alert alert-success"> ' + response.message + '</div>'
+        } else if (response.code == 2) {
+          this.target = '<div class="alert alert-warning">' + response.message + '</div>'
+        }
+      });
+    }
+
   }
 
 }
