2aeb2db94ff968b50589e4f02491db2174c01897
kajal <122118273+Kajalshukla-ka-i@users.noreply.github.com>
2024-01-09 17:53:13 +0530

    .

--- angLar/routes/api.php
+++ angLar/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('login',[ApiController::class, 'authenticate']);
+Route::post('register',[ApiController::class,'register']);
+
+Route::group(['middleware'=>['jwt.verify']],function(){
+    Route::get('logout',[ApiController::class,'logout']);
 });
