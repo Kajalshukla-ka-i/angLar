2d25077263739a8f83fb11dfb72a160fe1a72c1c
kajal <122118273+Kajalshukla-ka-i@users.noreply.github.com>
2024-01-09 16:46:32 +0530

    Update jwtMiddleware.php

--- angLar/app/Http/Middleware/jwtMiddleware.php
+++ angLar/app/Http/Middleware/jwtMiddleware.php
@@ -19,6 +19,28 @@ class jwtMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        /**
+         * Handle an incoming request.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+         * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+         */
+
+         try{
+            $user = JWTAuth::parseToken()->authenticate();
+         }catch(Exception $e){
+            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status'=>'Token is Invalid !']);
+
+            }elseif($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status'=>'Token is Expired !']);
+
+            }else{
+                return response()->json(['status'=>'Authorised Token not Found !']);
+
+            }
+         }
         return $next($request);
     }
 }
